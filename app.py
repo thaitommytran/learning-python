db88c2387442dd0bcd6553c5475091f3b8c75efd
Thai Tran <thai.tommy.tran@gmail.com>
2025-11-15 16:24:08 -0600

    added data types and string methods

--- app.py
+++ app.py
@@ -10,3 +10,20 @@ x = 1
 y = 2
 unit_price = 3
 
+# Data Types
+students_count = 1000
+rating = 4.99
+is_published = True
+course_name = "Python Programming"
+print(students_count)
+
+# String Methods
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
+
