e306b31702d1081c1d5b778aa9d5fd5490d2fe90
Thai Tran <thai.tommy.tran@gmail.com>
2025-11-17 22:19:43 -0600

    added depth first search

---
+++ depth_first_search.py
@@ -0,0 +1,32 @@
+graph = {
+    'A': ['B', 'G'],
+    'B': ['C', 'D', 'E'],
+    'C': [],
+    'D': [],
+    'E': ['F'],
+    'F': [],
+    'G': ['H'],
+    'H': ['I'],
+    'I': [],
+}
+
+
+def depth_first_search(graph, node):
+    visited = set()
+    stack = []
+
+    # append starting node
+    visited.append(node)
+    stack.append(node)
+
+    while stack:
+        s = stack.pop()
+        print(s, end=" ")
+
+        for n in reversed(graph[s]):
+            if n not in visited:
+                visited.append(n)
+                stack.append(n)
+
+
+depth_first_search(graph, "A")
