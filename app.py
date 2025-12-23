f00de70b352ef1b85f8645e7cc46693a60cbdb18
Thai Tran <thai.tommy.tran@gmail.com>
2025-12-23 12:56:46 -0600

    added types of functions, keyword and default arguments, and xargs

--- app.py
+++ app.py
@@ -279,3 +279,45 @@ def greet(first_name, last_name):
 
 greet("Tommy", "Tran")
 greet("John", "Smith")
+
+# Types of Functions
+# 1 - Perform a task
+# 2 - Return a value
+def get_greeting(name):
+    return f"Hi {name}" 
+
+message = get_greeting("Tommy")
+file = open("content.txt", "w")
+file.write(message)
+
+# Keyword Arguments
+def increment(number, by):
+    return number + by
+
+result = increment(2, 1)
+print(result)
+
+print(increment(2, by=1))
+
+# Default Arguments
+def increment(number, by=1):
+    return number + by
+
+print(increment(2))
+print(increment(2, 5))
+
+# Xargs
+def multiply(x, y):
+    return x * y
+
+multiply(2, 3)
+
+def multiply(*numbers):
+    total = 1
+
+    for number in numbers:
+        total *= number
+    
+    return total
+
+print(multiply(2, 3, 4, 5))
