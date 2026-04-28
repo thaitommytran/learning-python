48e3f01e95fff200224ad766813e8ac642a9a3dc
Thai Tran <thai.tommy.tran@gmail.com>
2026-04-28 16:21:15 -0500

    added variables, types, and excersise 2

--- scrimba/index.py
+++ scrimba/index.py
@@ -1,6 +1,46 @@
+spacer = '------------------'
+print(spacer)
+
+# Print Statements
 print('Welcome to Python 101!2')
 print('Welcome to Python 101!')
 
 print('Create hammer')
 print('Create nails')
 print('Use hammer and nails')
+print(spacer)
+
+# Variables and Type Casting
+failed_subjects = 6
+name = 'John'
+
+print('Dear Mrs Badger')
+print('Your son ' + name + ' is failing ' + str(failed_subjects) + ' subjects.')
+print(name + ' will need to redo ' + str(failed_subjects) + ' courses.')
+
+name = 'Eric'
+
+print(name + ' is doing well in geography.')
+print(spacer)
+
+# Data Types
+failed_subjects = 2.45
+name = 'John'
+Boolean = True or False
+a = "it's"
+b = 'it\'s'
+
+print(type('hello'))
+print(type(1))
+print(type(1.64))
+print(type(True))
+print(spacer)
+
+#Create appropriate Variables for Item name, the price 
+#and how many you have in stock
+item_name = "Toy"
+price = 3.50
+stock = 77
+
+is_in_inventory = True
+print(item_name, price, stock)
