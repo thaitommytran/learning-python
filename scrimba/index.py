0b49b12b67ce98938e6c74676baca4399fe3fe28
Thai Tran <thai.tommy.tran@gmail.com>
2026-04-29 16:41:13 -0500

    added exercise 3

--- scrimba/index.py
+++ scrimba/index.py
@@ -82,3 +82,27 @@ print("Passes Bought:", number_of_passes)
 print("Total Tokens:", total_tokens)
 print("Total Cost:", total_cost)
 print("Games Available:", games_available)
+print(spacer)
+
+# User Input
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+print("Hello, " + name + "! You are " + age + " years old.")
+
+num1 = input("Enter a digit: ")
+num2 = input("Enter a second digit: ")
+answer = float(num1) + float(num2)
+print("The sum is:", answer)
+print(spacer)
+
+# - Create a distance converter converting Km to miles
+# - Take two inputs from user: Their first name and the distance in km
+# - Print: Greet user by name and show km, and mile values
+# - 1 mile is 1.609 kilometers
+# - hint: use correct types for calculating and print
+# - Did you capitalize the name
+
+name = input("What is your name: ")
+distance_in_km = input("Enter distance in km: ")
+distance_in_miles = float(distance_in_km) / 1.609
+print(f"Hi {name.title()}! {distance_in_km} kilometers is equivalent to {round(distance_in_miles, 2)} miles.")
