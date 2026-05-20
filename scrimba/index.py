dfe00776c391f9afe55e86e393334a556adc169e
Thai Tran <thai.tommy.tran@gmail.com>
2026-05-20 11:20:28 -0500

    added exercise 8

--- scrimba/index.py
+++ scrimba/index.py
@@ -284,4 +284,14 @@ print(f"Sum of best and worst day: ${sum_best_worst:.2f}")
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-
+# Exercise 8: Split and Join
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+friends_list = ['Exercise: fill me with names']
+print(friends_list)
+# From the list above fill a list(friends_list) properly
+# with the names of all the friends. One per "slot"
+# you may need to run same command several times
+# use print() statements to work your way through the exercise
+    
+friends_list = (",".join((",".join((csv.split(":"))).split(";")))).split(",")
+print(friends_list)
